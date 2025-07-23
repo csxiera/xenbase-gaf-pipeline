3b657d78be6e4db25fec7ceeb0ba6e8ae377b4b1
Courtney Lenz <courtney.lenz@ucalgary.ca>
2025-07-23 15:38:32 -0600

    Added gz compression step for output files

--- scripts/get_files.sh
+++ scripts/get_files.sh
@@ -11,43 +11,43 @@ mkdir -p $GAF_DIR/original-goa
 mkdir -p $NCBI_MAP_DIR
 
 # Download goa data for all species
-curl -o $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz https://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/goa_uniprot_all.gaf.gz
-echo "Full GOA downloaded"
+#curl -o $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz https://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/goa_uniprot_all.gaf.gz
+#echo "Full GOA downloaded"
 
 # Extract into species specific files
 echo "Starting extraction of species-specific annotations from $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz"
-zgrep -a 'taxon:8355\|taxon:8364' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Xenopus.GOA.Extracted.$DATE.gaf
-echo "Xenopus GOA Extracted"
-zgrep -a 'taxon:9606' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Human.GOA.Extracted.$DATE.gaf
-echo "Human GOA Extracted"
-zgrep -a 'taxon:10090' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Mouse.GOA.Extracted.$DATE.gaf
-echo "Mouse GOA Extracted"
-zgrep -a 'taxon:10116' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Rat.GOA.Extracted.$DATE.gaf
-echo "Rat GOA Extracted"
-zgrep -a 'taxon:9031' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Chicken.GOA.Extracted.$DATE.gaf
-echo "Chicken GOA Extracted"
+#zgrep -a 'taxon:8355\|taxon:8364' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Xenopus.GOA.Extracted.$DATE.gaf
+#echo "Xenopus GOA Extracted"
+#zgrep -a 'taxon:9606' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Human.GOA.Extracted.$DATE.gaf
+#echo "Human GOA Extracted"
+#zgrep -a 'taxon:10090' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Mouse.GOA.Extracted.$DATE.gaf
+#echo "Mouse GOA Extracted"
+#zgrep -a 'taxon:10116' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Rat.GOA.Extracted.$DATE.gaf
+#echo "Rat GOA Extracted"
+#zgrep -a 'taxon:9031' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Chicken.GOA.Extracted.$DATE.gaf
+#echo "Chicken GOA Extracted"
 zgrep -a 'taxon:7955' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Zebrafish.GOA.Extracted.$DATE.gaf
 echo "Zebrafish GOA Extracted"
-zgrep -a 'taxon:7227' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Drosophila.GOA.Extracted.$DATE.gaf
-echo "Drosophila GOA Extracted"
+#zgrep -a 'taxon:7227' $GAF_DIR/original-goa/goa_uniprot_all.gaf.gz > $GAF_DIR/Drosophila.GOA.Extracted.$DATE.gaf
+#echo "Drosophila GOA Extracted"
 
 # Download Xenbase gpi, genepage to gene ID map, and gene ID to ortholog NCBI id map
-curl https://download.xenbase.org/xenbase/GenePageReports/xenbase.gpi.gz | gunzip > $XB_DIR/Xenbase.gpi
-curl https://download.xenbase.org/xenbase/GenePageReports/XenbaseGenepageToGeneIdMapping_chd.txt > $XB_DIR/Xenbase_Genepage_To_GeneId.txt
+#curl https://download.xenbase.org/xenbase/GenePageReports/xenbase.gpi.gz | gunzip > $XB_DIR/Xenbase.gpi
+#curl https://download.xenbase.org/xenbase/GenePageReports/XenbaseGenepageToGeneIdMapping_chd.txt > $XB_DIR/Xenbase_Genepage_To_GeneId.txt
 # NOTE: ^ line 11651 may be uncorrectly tabbed ("vma22  .L" instead of vma22.L); fix before using if redownloading
-curl https://xenbase-bio1.ucalgary.ca/cgi-bin/reports/genepage_entrez_orthologs.cgi | sed '/./,$!d' > "$NCBI_MAP_DIR/Xenopus_NCBI_Orthologs.tsv"
-{ echo -e "umbrella_id\txtrop\thuman\tmouse\trat\tzebrafish\tchicken\tdrosophila\tworm"; tail -n +2 "$NCBI_MAP_DIR/Xenopus_NCBI_Orthologs.tsv"; } > "$NCBI_MAP_DIR/tmp" && mv "$NCBI_MAP_DIR/tmp" "$NCBI_MAP_DIR/Xenopus_NCBI_Orthologs.tsv"
+#curl https://xenbase-bio1.ucalgary.ca/cgi-bin/reports/genepage_entrez_orthologs.cgi | sed '/./,$!d' > "$NCBI_MAP_DIR/Xenopus_NCBI_Orthologs.tsv"
+#{ echo -e "umbrella_id\txtrop\thuman\tmouse\trat\tzebrafish\tchicken\tdrosophila\tworm"; tail -n +2 "$NCBI_MAP_DIR/Xenopus_NCBI_Orthologs.tsv"; } > "$NCBI_MAP_DIR/tmp" && mv "$NCBI_MAP_DIR/tmp" "$NCBI_MAP_DIR/Xenopus_NCBI_Orthologs.tsv"
 # NOTE: ^ Changed header in ortholog file to match species names used in program/filenames
-echo "Xenbase files downloaded"
+#echo "Xenbase files downloaded"
 
 # Download uniprot to NCBI id mapping files for each ortholog species
-curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Human_NCBI_Mapping.tsv
-curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/MOUSE_10090_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Mouse_NCBI_Mapping.tsv
-curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/RAT_10116_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Rat_NCBI_Mapping.tsv
-curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/CHICK_9031_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Chicken_NCBI_Mapping.tsv
-curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/DANRE_7955_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Zebrafish_NCBI_Mapping.tsv
-curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/DROME_7227_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Drosophila_NCBI_Mapping.tsv
-echo "NCBI maps downloaded"
+#curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Human_NCBI_Mapping.tsv
+#curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/MOUSE_10090_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Mouse_NCBI_Mapping.tsv
+#curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/RAT_10116_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Rat_NCBI_Mapping.tsv
+#curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/CHICK_9031_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Chicken_NCBI_Mapping.tsv
+#curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/DANRE_7955_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Zebrafish_NCBI_Mapping.tsv
+#curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/DROME_7227_idmapping.dat.gz | gunzip | grep 'GeneID' > $NCBI_MAP_DIR/Drosophila_NCBI_Mapping.tsv
+#echo "NCBI maps downloaded"
 
 # - - - - - - - - - - - - - - - - - - - - Not Used - - - - - - - - - - - - - - - - - - - -
 
